87c0282a018526969ccf3a53932a90f58ece6efb
Joe <joegezo@gmail.com>
2025-08-02 15:33:28 -0400

    bug fix for non-nested run case

--- bishop/_mlflow.py
+++ bishop/_mlflow.py
@@ -20,7 +20,8 @@ def get_runs_as_json(experiment, mapping, round_to=None):
     df = mlflow.search_runs(experiment_names=[experiment])
     output = []
     for e,r in df.iterrows():
-        if r['tags.mlflow.parentRunId'] is None:
+        #if r['tags.mlflow.parentRunId'] is None:
+        if r.get('tags.mlflow.parentRunId', None) is None:
             output.append(
                 {mapping[k]:_round(r[k]) for k in mapping}
             )

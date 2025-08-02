45599ada072ce7c46720fa7053fa38a286048e2d
Joe <joegezo@gmail.com>
2025-08-02 12:19:31 -0400

    new module for analyst object, and updated pandas tool to give more explicit feedback

--- bishop/_analyst.py
+++ bishop/_analyst.py
@@ -9,8 +9,9 @@ All methods in the pandas.DataFrame object:
 df_functions = [name for name, obj in inspect.getmembers(df, predicate=inspect.ismethod)]
 len(df_functions)
 """
-
+import pandas as pd
 import dspy
+import typing
 
 
 
@@ -40,25 +41,42 @@ DF_WHITELIST = ['abs', 'add', 'add_prefix', 'add_suffix', 'agg', 'aggregate', 'a
                 'subtract', 'sum', 'swapaxes', 'swaplevel', 'tail', 'take', 'transform', 'transpose', 'truediv',
                 'truncate', 'tz_convert', 'tz_localize', 'unstack', 'update', 'value_counts', 'var', 'where', 'xs']
 
+
+foo = """
+        Call any function in the pandas API, starting with 'pd.' or 'df.' on the 
+        dataframe 'df' and retrieve results. lambda functions and df.eval() are 
+        not allowed!! 
+        The query must be a single call to the pandas API; multiple lines combined
+        with a semicolon will return an error. Intermediate steps like assigning a 
+        new column won't work.
+        """
+
 def return_pandas_query_tool(df, strict=False):
     def pandas_query(command:str) -> str:
         """
-        Call any function in the pandas API, starting with 'pd.' or 'df.' on the 
-        dataframe 'df' and retrieve results. lambda functions and df.eval() are 
-        not allowed!! The code will be run using eval(), so intermediate steps 
-        like assigning a new column won't work.
+        Query the dataset using pandas
         """
+        
         _ = df.describe()
         exec("import pandas as pd")
-        #print("\ncommand:", command)
-        if ("lambda" in command.lower())|("pd.eval" in command.lower()):
-            result = "command not allowed!"
+        print("\ncommand:", command)
+        if "import" in command.lower():
+            result = "FAIL: import statements not allowed!"
+        elif "lambda" in command.lower():
+            result = "FAIL: command 'lambda' not allowed!"
+        elif "pd.eval" in command.lower():
+            result = "FAIL: command 'eval' not allowed!"
+        elif "np." in command:
+            result = "FAIL: numpy calls not allowed; only pandas"
+        #if ("lambda" in command.lower())|("pd.eval" in command.lower()):
+        #    result = "command not allowed!"
         elif ";" in command:
-            result = "multi-line commands not allowed!"
-        elif not command.startswith("pd.")|command.startswith("df")|command.startswith("(pd.")|command.startswith("(df"):
-            result ="command not allowed! must start with `pd.` or `df.`"
+            result = "FAIL: multiple statements not allowed!"
+        # remove "(" for this test because "((df['y'] - df['x']**2)**2).mean()" is OK
+        elif not command.replace("(","").startswith("pd.")|command.replace("(","").startswith("df"):
+            result ="FAIL: command not allowed! must start with `pd.` or `df.`"
         elif command.startswith("pd.read")|command.startswith("pd.to_"):
-            result = "not allowed to read from or write to disk"
+            result = "FAIL: not allowed to read from or write to disk"
         else:
             try:
                 allowed = True
@@ -75,14 +93,61 @@ def return_pandas_query_tool(df, strict=False):
                             #if not all(char.isdigit() for char in func):
                             if func not in PD_WHITELIST+DF_WHITELIST:
                                 allowed = False
-                                result = f"function {func} not permitted"
+                                result = f"FAIL: function {func} not permitted"
                 if allowed:
                     result = eval(command)
             except Exception as e:
-                result = f"Command failed with this error: {e}"
-        #print("result:", result)
+                result = f"FAIL: Command failed with this error: {e}"
+        print("result:", result)
         return result
     return pandas_query
+
+
+def _pandas_query(command:str, df:pd.core.frame.DataFrame, strict:bool=True) -> str:
+    """
+    Query the dataset using pandas
+    """
+    failures = []
+    _ = df.describe() # still need this?
+    exec("import pandas as pd") # still need this?
+    print("\ncommand:", command)
+    if "import" in command.lower():
+        failures.append("import statements not allowed")
+    if "lambda" in command.lower():
+        failures.append("lambda commands not allowed")
+    if "pd.eval" in command.lower():
+        failures.append("eval comands not allowed")
+    if "np." in command:
+        failures.append("numpy commands not allowed; only pandas")
+    if ";" in command:
+        failures.append("multiple lines connected by `;` not allowed")
+    if not command.replace("(","").startswith("pd.")|command.replace("(","").startswith("df"):
+        failures.append("command not allowed! must start wtih `pd.` or `df.`")
+    if command.startswith("pd.read")|command.startswith("pd.to_"):
+        failures.append("not allowed to read from or write to disk")
+    # strict mode- find everything that looks like a function and check to 
+    # see if it's whitelisted
+    if strict:
+        # split out every case that looks like pd.FUNCTION(), df.FUNCTION(), 
+        # df["colum_name"].FUNCTION(), etc
+        for f in command.split(".")[1:]:
+            # if it's a function call there should be a parenthesis- without this
+            # check, the function will flag on decimals
+            if "(" in f:
+                func = f.split("(")[0]
+                if func not in PD_WHITELIST+DF_WHITELIST:
+                    failures.append(f"function {func} not permitted")
+    if len(failures) == 0:
+        try:
+            result = eval(command)
+        except Exception as e:
+            failures.append("error: {e}")
+    if len(failures) > 0:
+        result = "Command failed for the following reasons:"
+        for f in failures:
+            result += f"\n* {f}"
+        result += "\n**Please reframe your query or ask a different question.**"
+    return result
 
 
 class AnalystSig(dspy.Signature):
@@ -92,7 +157,22 @@ class AnalystSig(dspy.Signature):
 
     Input a question about a dataset along with the background/context for the question. Return 
     your best answer, including caveats and an explanation of your confidence in the 
-    assessment. You can use your python skills to analyze the dataset using the pandas API.
+    assessment. You can use your python skills to analyze the dataset using single-line calls
+    to the pandas API.
+
+    If you get a "Command failed" error, try asking a different question! Do not import anything,
+    do not try to make multiple calls with a ";", don't create new columns, and do not use 
+    "eval" or "lambda".
+
+    Example acceptable calls:
+    df["foo"].mean()
+    df.groupby(["foo", "bar"]).size()
+
+    Example unacceptable calls:
+    df["bar"] = df["foo"]+1
+    data = df[["foo", "bar"]]; means = data.mean()
+    df["foo"].plot()
+    df["foo"].apply(lambda x: x**2)
     """
     background:str = dspy.InputField()
     question:str = dspy.InputField()
@@ -110,3 +190,52 @@ def get_analyst(df, strict=True, max_iters=25):
     """
     return dspy.ReAct(AnalystSig, tools=[return_pandas_query_tool(df.copy(), strict=strict)], 
                       max_iters=max_iters)
+
+
+class Analyst(dspy.Module):
+    """
+    General-purpose data analysis agent
+    """
+    def __init__(self, max_iters:int=25, strict:bool=True,
+                 df:typing.Union[None,pd.core.frame.DataFrame]=None,
+                 verbose:bool=False):
+        """
+        :max_iters: max number of ReAct iterations to query dataset for analysis
+        :strict: if True, only permit explicitly whitelisted pandas functions
+        :df: pandas DataFrame to use for analysis
+        :verbose: if True, print out each stage of analysis
+        """
+        self.max_iters = max_iters
+        self.strict = strict
+        self.df = df
+        self.verbose = verbose
+        self.react = dspy.ReAct(AnalystSig, tools=[self.pandas_query], 
+                      max_iters=max_iters)
+        
+    def update_dataframe(self, df=pd.core.frame.DataFrame):
+        self.df = df
+
+    def pandas_query(self, command:str) -> str:
+        """
+        Use a single line of pandas code to probe the dataset
+        """
+        if self.verbose:
+            print(f"analyst command: {command}")
+        result = _pandas_query(command, self.df, strict=self.strict)
+        # this is where we could potentially change the output when the LLM
+        # keeps repeating a failed query
+        if self.verbose:
+            print(f"analyst response: {result}")
+        return result
+    
+    def forward(self, question:str, background:str="None", 
+                df:typing.Union[None,pd.core.frame.DataFrame]=None) -> dspy.Prediction:
+        """
+        do analysis
+        """
+        if df is not None:
+            self.update_dataframe(df)
+          
+        return self.react(question=question,
+                          background=background, 
+                          description=self.df.describe().to_markdown())

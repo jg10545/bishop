5ecfdffd5f9bba0715878ee37b9d92290f8eb6a7
Joe <joegezo@gmail.com>
2025-08-05 17:08:23 -0400

    docs and code cleanup

--- bishop/_main.py
+++ bishop/_main.py
@@ -23,16 +23,38 @@ PRICING = {
 }
 
 
-
 class Laboratory(dspy.Module):
     """
+    This class runs fully automated in silico research.
+
+    You can configure the details of the research program using the experiment function and prompts. To change
+    the workflow that the laboratory follows, subclass Laboratory and overwrite two functions:
+
+    * setup() initializes all the agents and any other data structures you need
+    * run_one_experiment() runs a single end-to-end experiment.
+
     For now assume we're using the same LLM for everything. Could potentially make that configurable in the
     future, but would require changing a few things.
+
+    Before creating the Laboratory object:
+    * initialize a dspy.LM object for the LLM you're using
+    * call dspy.configure(lm=lm, track_usage=True)
+    * configure MLFlow with mlflow.set_tracking_uri() and mlflow.set_experiment()
     """
     def __init__(self, lm, experiment_fn, experiment_name, metric_name, prompts, human_in_loop:True, verbose=False):
         """
+        :lm: dspy.LM object; the language model used by the agents in this experiment
+        :experiment_fn: python function that handles all the details of running the actual experiment.
+            * It should input a string containing the LLM-written python function for this run
+            * It should output a dictionary containing the output metric and "df", a pandas dataframe of results to send
+                to the analyst agent
+        :experiment_name: string; name of the mlflow experiment
+        :metric_name: string; name of the performance metric to be maximized/minimized
+        :prompts: dictionary of contextual prompts for the different agents. By default this should include things like
+            the background for the experiment and constraints for the python function
+        :human_in_loop: bool; if True, require human review before running the machine-generated experiment code
+        :verbose: bool; if True, print stuff out at every stage.
         """
-        #dspy.configure(track_usage=True, lm=lm)
         self.lm = lm
         self.model = lm.model
         self.experiment_name = experiment_name
@@ -52,25 +74,37 @@ class Laboratory(dspy.Module):
         Initialize all the agents and any other metadata we'll need. To customize the lab workflow, subclass 
         Laboratory and overwrite this function and run_one_experiment(). Add each agents to the self.agents 
         dictionary.
+
+        This is also a good place to check the prompts the user passes to make sure the right stuff is included.
         """
         # create each agent we'll need
         self.agents["ideator"] = dspy.ChainOfThought(IdeatorSig)
         self.agents["planner"] = dspy.ChainOfThought(PlannerSig)
         self.agents["coder"] = Coder(human_in_loop=self.human_in_loop, verbose=self.verbose)
         self.agents["analyst"] = Analyst(verbose=self.verbose)
-        # identify the columns we'll need from mlflow to report on the 
+        # identify the columns we'll need from mlflow to report on the history of the experiments
         self.mlflow_column_mapping = {
-            #"params.hypothesis":"hypothesis",
             "params.planner.final_hypothesis":"hypothesis",
             "params.planner.title":"title",
             f"metrics.{self.metric_name}":f"{self.metric_name}",
-            #"params.analysis":"analysis",
             "params.analyst.answer":"analysis",
             "tags.status":"status",
             "tags.comment":"comment"
         }
         for k in ["background", "analysis_question", "function_name", "constraints"]:
             assert k in self.prompts, f"Missing prompt {k}"
+
+        description = f"""
+        # Background
+        {self.prompts["background"]}
+
+        # Analysis Question
+        {self.prompts["result_analysis_question"]}
+
+        # Implementation Constraints
+        {self.prompts["implementation_constraints"]}
+        """
+        mlflow.set_experiment_tag("mlflow.note.content", self._get_description())
 
 
     def run_one_experiment(self, **kwargs):
@@ -122,28 +156,28 @@ class Laboratory(dspy.Module):
         analysis = self._call_agent("analyst", df=results["df"],
                                         question=p["analysis_question"],
                                         background=p["background"])
-        print("analysis keys:", analysis.keys())
         outdict["analysis"] = analysis.answer
         return outdict
 
 
     def _call_agent(self, name, **kwargs):
+        """
+        Wrapper function for calling an agent; handles some additional logging and stuff
+        """
         # run inputs through the agent
         with track_usage() as usage_tracker:
             outputs = self.agents[name](**kwargs)
-        #outputs.set_lm_usage(usage_tracker.get_total_tokens())
         self.usage[name] = usage_tracker.get_total_tokens()
         # log every output to MLflow
         for k in outputs.keys():
             if k != "trajectory":
                 self.log_param(f"{name}.{k}", outputs[k])
-        # track usage statistics
-        #self.usage[name] = outputs.get_lm_usage()
         return outputs
 
     def _log_usage(self):
         """
         Log total token usage as mlflow metrics and usage broken out by agent as an artifact.
+        Also make some estimates of what it would cost to run this through several hosted APIs.
         """
         completion_tokens = 0
         prompt_tokens = 0
@@ -186,130 +220,3 @@ class Laboratory(dspy.Module):
             logging.warning(f"parameter {key} is above the max token limit for MLFlow. Recording only the first {MLFLOW_PARAM_TOKEN_LIMIT} characters.")
         mlflow.log_param(key, value[:MLFLOW_PARAM_TOKEN_LIMIT])
 
-
-class Experimenter(dspy.Module):
-    """
-    
-    """
-
-    def __init__(self, experiment_fn:Callable[str,dict], background:str, metric_name:str, 
-                 result_analysis_question:str, implementation_constraints:str, 
-                 mlflow_tracking_uri:str, experiment_name:str,
-                 analyst_max_iters:int=25, 
-                 implementer_max_iters:int=25, human_in_loop:bool=True, strict:bool=True,
-                 function_name:str="test_fn"):
-        """
-        :experiment_fn: python function that inputs a string containing code and outputs a 
-            dictionary of results. This is where the actual experiment happens!
-        :background: string; prompt giving the background/context/goal of the project
-        :metric_name: string; name of the success metric. Should appear in the experiment_fn 
-            output.
-        :result_analysis_question: string; prompt to pass an Analyst agent for reviewing the
-            results of an experiment
-        :implementation_constraints: string; prompt to give a Coder agent to constrain how it
-            implements a function for the next experiment.
-        :analyst_max_iters: int; number of times Analyst agent can query the dataset
-        :implementer_max_iters: int; number of times Coder agent can try writing code for the 
-            next experiment. Current run will end if unable to pass automated checks this many 
-            times.
-        :human_in_loop: bool; if True, require human review before running the Coder agent's
-            code
-        :strict: bool; if True, restrict the Analyst agent to only use a manually-whitelisted
-            subset of the pandas API
-        :function_name: string; name of the function Coder agent should write.
-        """
-        self.experiment_fn = experiment_fn
-        self.background = background
-        self.metric_name = metric_name
-        self.result_analysis_question = result_analysis_question
-        self.implementation_constraints = implementation_constraints
-        self.experiment_name = experiment_name
-        self.analyst_max_iters = analyst_max_iters
-        self.implementer_max_iters = implementer_max_iters
-        self.human_in_loop = human_in_loop
-        self.strict = strict
-        self.function_name = function_name
-
-        mlflow.set_tracking_uri(mlflow_tracking_uri)
-        mlflow.set_experiment(experiment_name)
-        mlflow.set_experiment_tag("mlflow.note.content", self._get_description())
-
-        self.planner = get_planner()
-        self.implementer = get_implementer(max_iters=implementer_max_iters)
-
-        self.mlflow_column_mapping = {
-            "params.hypothesis":"hypothesis",
-            f"metrics.{metric_name}":f"{metric_name}",
-            "params.analysis":"analysis",
-            "tags.status":"status",
-            "tags.comment":"comment"
-        }
-
-    def _get_description(self):
-        return f"""
-        # Background
-        {self.background}
-
-        # Analysis Question
-        {self.result_analysis_question}
-
-        # Implementation Constraints
-        {self.implementation_constraints}
-        """
-
-    def log_param(self, key, value):
-        if len(value) > MLFLOW_PARAM_TOKEN_LIMIT:
-            logging.warning(f"parameter {key} is above the max token limit for MLFlow. Recording only the first {MLFLOW_PARAM_TOKEN_LIMIT} characters.")
-        mlflow.log_param(key, value[:MLFLOW_PARAM_TOKEN_LIMIT])
-
-
-    def forward(self, 
-                hypothesis:Union[str,None]=None,
-                code:Union[str,None]=None) -> dspy.Prediction:
-        """
-        
-        """
-        with mlflow.start_run():
-            mlflow.set_tag("status", "incomplete")
-            mlflow.set_tag("comment", "none")
-            mlflow.log_param("model", dspy.settings.get("lm").model)
-            if hypothesis is None:
-                # pull history
-                history = get_runs_as_json(self.experiment_name, self.mlflow_column_mapping, round_to=3)
-                hyp = self.planner(background=self.background,
-                                   history=json.dumps(history))
-                self.log_param("hypothesis_cot", hyp.reasoning)
-                hypothesis = hyp.hypothesis
-            self.log_param("hypothesis", hypothesis)
-            if code is None:
-                # Implementer inputs background and hypothesis and writes code   
-                implementation = self.implementer(background=self.background,
-                                                  hypothesis=hypothesis,
-                                                  constraints=self.implementation_constraints,
-                                                  function_name=self.function_name)
-                code = _strip_markdown_from_code(implementation.code)
-                # If human-in-loop enabled, get permission to run AFTER Implementer is finished
-                codecheck = code_checker(code, self.human_in_loop)
-                if codecheck != "pass":
-                    mlflow.set_tag("status", "fail")
-                    raise Exception(f"generated code did not pass final check: {codecheck}")
-            self.log_param("code", code)
-            # run experiment loop with code
-            try:
-                results = self.experiment_fn(code)
-            except Exception as e:
-                mlflow.set_tag("status", "fail")
-                mlflow.log_param("error_message", e)
-                return False
-            mlflow.log_metric(self.metric_name, results[self.metric_name])
-            # pull detailed results from this run
-            if "df" in results:
-                q = f"{self.result_analysis_question}\nHypothesis: {hypothesis}"
-                analyst = get_analyst(results["df"], strict=self.strict, max_iters=self.analyst_max_iters)
-                result_analysis = analyst(question=q, 
-                                          background=self.background, 
-                                          description=results["df"].describe().to_markdown())
-                self.log_param("analysis", result_analysis.answer)
-            
-            mlflow.set_tag("status", "complete")
-        return dspy.Prediction(hypothesis=hypothesis, code=code, analysis=result_analysis.answer)
